26580cbacc27af6b909fd518c4109a292be92a07
message-deleted <229983@edu.p.lodz.pl>
2021-11-29 19:37:28 +0100

    Initial commit

---
+++ src/main/java/p/lodz/pl/pas/HelloApplication.java
@@ -0,0 +1,9 @@
+package p.lodz.pl.pas;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
