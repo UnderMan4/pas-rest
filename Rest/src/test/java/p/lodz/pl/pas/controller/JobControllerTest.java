d540342ebe86dafe57cae98ebb538aa789d9563f
Filip Grzechnik 229888 <229888@edu.p.lodz.pl>
2022-01-02 16:32:19 +0100

    new structure

---
+++ Rest/src/test/java/p/lodz/pl/pas/controller/JobControllerTest.java
@@ -0,0 +1,102 @@
+package p.lodz.pl.pas.controller;
+
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+// import io.restassured.internal.http.URIBuilder;
+import io.restassured.matcher.RestAssuredMatchers;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import java.net.URISyntaxException;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class JobControllerTest {
+    
+    ClientBuilder clientBuilder;
+    Client client;
+    WebTarget target;
+    String created_uuid;
+    String url = "http://localhost:8080/api/job";
+
+    public JobControllerTest() throws URISyntaxException {
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        clientBuilder = ClientBuilder.newBuilder();
+        client = clientBuilder.build();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCreateJob() {
+    }
+
+    @Test
+    public void testGetList() throws URISyntaxException {
+
+        // JSONObject json = new JsonObject();
+        // Gson json = new Gson();
+        // json.fromJson("""
+        //         [
+        //             {
+        //                 "login":"jkowalski",
+        //                 "name":"Jan",
+        //                 "surname":"Kowalski",
+        //                 "uuid":"54ceb043-5f89-41bb-a29b-f2c0e9909dad",
+        //                 "active":true,
+        //                 "accessLevel":"User"
+        //             },
+        //             {
+        //                 "login":"jjjkowal",
+        //                 "name":"Jaroslaw",
+        //                 "surname":"Kowalski",
+        //                 "uuid":"40d68ba5-39ba-4f2e-9e61-a55daf7b3e8e",
+        //                 "active":true,
+        //                 "accessLevel":"ResourceAdministrator"
+        //             },
+        //             {
+        //                 "login":"ttttt",
+        //                 "name":"Tomasz",
+        //                 "surname":"Kowalski",
+        //                 "uuid":"84d267cf-6dc4-40cd-b1d3-000733a85458",
+        //                 "active":true,
+        //                 "accessLevel":"User"
+        //             },
+        //             {
+        //                 "login":"Restitutor",
+        //                 "name":"Lucius",
+        //                 "surname":"Aurelianus",
+        //                 "uuid":"295eea09-5541-42e4-ac24-126a0d87607e",
+        //                 "active":true,
+        //                 "accessLevel":"UserAdministrator"
+        //             }
+        //         ]
+        //         """, '');
+        // RestAssured.get(url + "/list").then()
+        //         .statusCode(202).assertThat()
+        //         .body("", equalTo());
+    }
+
+    @Test
+    public void testFindJob() {
+    }
+
+    @Test
+    public void testUpdateJob() {
+    }
+
+    @Test
+    public void testRemoveJob() {
+    }
+}
