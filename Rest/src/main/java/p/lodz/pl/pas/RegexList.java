28474ecb9b9d14c6d47409543e88639b03cc3850
message-deleted <229983@edu.p.lodz.pl>
2022-01-12 18:52:57 +0100

    Update RegexList.java

--- Rest/src/main/java/p/lodz/pl/pas/RegexList.java
+++ Rest/src/main/java/p/lodz/pl/pas/RegexList.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class RegexList {
 
-    public static Pattern USERNAME = Pattern.compile("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
+    public static Pattern USERNAME = Pattern.compile("^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
     public static Pattern JOB_NAME = Pattern.compile("^(?=.{8,20}$)(?![_. ])(?!.*[_. ]{2})[a-zA-Z0-9._ ]+(?<![_. ])$");
 
     // Regex for description ex. Job description
