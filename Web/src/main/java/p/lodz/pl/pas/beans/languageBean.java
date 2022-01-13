5cfc3c026116dd89c44cf17ced70caf5fc1ab331
message-deleted <229983@edu.p.lodz.pl>
2022-01-13 09:47:42 +0100

    Language changing works

---
+++ Web/src/main/java/p/lodz/pl/pas/beans/languageBean.java
@@ -0,0 +1,40 @@
+package p.lodz.pl.pas.beans;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Locale;
+
+@Named
+@SessionScoped
+public class languageBean implements Serializable {
+    private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+    public synchronized void changeLanguage() {
+        if (locale.getLanguage().equals("pl")) {
+            locale = new Locale("en");
+        } else {
+            locale = new Locale("pl");
+        }
+    }
+}
