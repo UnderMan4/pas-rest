f462bc99bf3a95b28912134c221b648ad40b833d
UnderMan4 <filiptheg@gmail.com>
2022-02-02 18:35:06 +0100

    Auto stash before rebase of "origin/main"

---
+++ Web/src/main/java/p/lodz/pl/pas/beans/LoginBean.java
@@ -0,0 +1,9 @@
+package p.lodz.pl.pas.beans;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+public class LoginBean implements Serializable {
+
+    private final Logger LOGGER = Logger.getLogger(getClass().getName());
+}
