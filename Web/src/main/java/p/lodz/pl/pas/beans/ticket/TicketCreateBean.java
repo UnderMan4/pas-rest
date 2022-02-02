ffd961623c7520a10be9aaba2df95ffc093c42f2
UnderMan4 <filiptheg@gmail.com>
2022-02-02 13:38:34 +0100

    create validation

--- Web/src/main/java/p/lodz/pl/pas/beans/ticket/TicketCreateBean.java
+++ Web/src/main/java/p/lodz/pl/pas/beans/ticket/TicketCreateBean.java
@@ -1,7 +1,10 @@
 package p.lodz.pl.pas.beans.ticket;
 
 import p.lodz.pl.pas.exceptions.RESTException;
+import p.lodz.pl.pas.model_web.Job;
+import p.lodz.pl.pas.model_web.Ticket;
 import p.lodz.pl.pas.model_web.TicketDTO;
+import p.lodz.pl.pas.model_web.User;
 import p.lodz.pl.pas.services.TicketService;
 
 import javax.enterprise.context.SessionScoped;
@@ -20,6 +23,9 @@ public class TicketCreateBean implements Serializable {
 
     private final TicketDTO newTicket = new TicketDTO();
 
+    private User user;
+    private Job job;
+
     public TicketCreateBean() {
         newTicket.setJobEnd(null);
     }
@@ -28,8 +34,26 @@ public class TicketCreateBean implements Serializable {
         return newTicket;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
     public String createNewTicket() {
         try {
+            newTicket.setJob(job.getUuid());
+            newTicket.setUser(user.getUuid());
             FacesMessage message = ticketService.createTicket(newTicket);
             FacesContext.getCurrentInstance().addMessage(null, message);
         } catch (RESTException e) {
